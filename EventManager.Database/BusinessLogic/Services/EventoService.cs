a259ac1462acdaa7ae670324404a5946dbc1c2d6
Alec Demian Santana Celaya <73375255+SantanaAlec@users.noreply.github.com>
2023-10-21 17:20:57 -0700

    Perform validation for Sala reservation overlaps

--- EventManager.Database/BusinessLogic/Services/EventoService.cs
+++ EventManager.Database/BusinessLogic/Services/EventoService.cs
@@ -1,7 +1,6 @@
 // Copyright (c) Miguel Angel De La Rosa Martínez, Alec Demian Santana Celaya, Jaime Valdez Tanori, Martin Ricardo Yocupicio Ramos. Licensed under the MIT Licence.
 // See the LICENSE file in the repository root for full license text.
 
-using EventManager.Database.DataAccess;
 using EventManager.Database.DataAccess.Repositories;
 using EventManager.Database.Models.Entities;
 
@@ -9,14 +8,11 @@ namespace EventManager.Database.BusinessLogic.Services
 {
     public partial class EventoService
     {
-        private readonly DatabaseContext _context;
-
         private readonly EventoRepository _eventoRepository;
 
-        public EventoService(DatabaseContext context)
+        public EventoService(EventoRepository eventoRepository)
         {
-            _context = context;
-            _eventoRepository = new EventoRepository(context);
+            _eventoRepository = eventoRepository;
         }
 
         public async Task<List<Evento>> GetAllAsync()
@@ -75,39 +71,12 @@ namespace EventManager.Database.BusinessLogic.Services
 
         private bool EventoOverlaps(Evento newEvento)
         {
-            //TODO: Corregir este metodo para que se puedan reservar a futuro; Debe checar si empalma con una sala que no este disponible para cuando se registre
-            return _context.Eventos
-                .Any(e => e.Id != newEvento.Id &&
-                          e.FechaInicio < newEvento.FechaTermino &&
-                          e.FechaTermino > newEvento.FechaInicio);
+            return _eventoRepository.EventoOverlaps(newEvento);
         }
 
         private bool ReservedAgregablesExceedTotal(Evento newEvento)
         {
-            //TODO: Checar que se liberen o que haya agregables disponibles para el momento de registrar un evento y que no solo se riga por el total, sino tambien por periodos
-            var totalReservedAgregables = _context.EventoAgregables
-                .Where(ea => ea.EventoId != newEvento.Id)
-                .GroupBy(ea => ea.AgregableId)
-                .Select(group => new
-                {
-                    AgregableId = group.Key,
-                    ReservedTotal = group.Sum(ea => ea.CantidadReservada)
-                })
-                .ToList();
-
-            foreach (EventoAgregable ea in newEvento.EventoAgregables)
-            {
-                var matchingReserved = totalReservedAgregables.FirstOrDefault(e => e.AgregableId == ea.AgregableId);
-                if (matchingReserved != null &&
-                    ea.CantidadReservada + matchingReserved.ReservedTotal > ea.Agregable.Total)
-                {
-                    // Reserved Agregables exceed the total available for at least one Agregable.
-                    return true;
-                }
-            }
-
-            // No Agregable exceeds the total available.
-            return false;
+            return _eventoRepository.ReservedAgregablesExceedTotal(newEvento);
         }
     }
 }
--- EventManager.Database/DataAccess/Repositories/EventoRepository.cs
+++ EventManager.Database/DataAccess/Repositories/EventoRepository.cs
@@ -64,5 +64,65 @@ namespace EventManager.Database.DataAccess.Repositories
                 .ThenInclude(ea => ea.Agregable)
                 .ToListAsync();
         }
+
+
+        public bool EventoOverlaps(Evento newEvento)
+        {
+            return _context.Eventos
+                .Any(evento =>
+                    // Don't let it check itself
+                    evento.Id != newEvento.Id &&
+                    // Check if period overlaps
+                    evento.FechaInicio < newEvento.FechaTermino &&
+                    evento.FechaTermino > newEvento.FechaInicio &&
+                    // Check if a Sala is already reserved for another Evento that will occur during the same period
+                    evento.Salas.Any(sala =>
+                        newEvento.Salas.Any(newEventoSala => newEventoSala.Id == sala.Id)));
+        }
+
+        public bool ReservedAgregablesExceedTotal(Evento newEvento)
+        {
+            // Awgh.
+            // This check should ONLY apply to Eventos that overlap with the new Evento, since otherwise all
+            // Agregables should be available either way
+            List<int> overlappingEventoIds = _context.Eventos
+                .Where(e =>
+                    e.Id != newEvento.Id &&
+                    e.FechaInicio < newEvento.FechaTermino &&
+                    e.FechaTermino > newEvento.FechaInicio)
+                .Select(e => e.Id)
+                .ToList();
+
+            var totalReservedAgregables =
+                _context.EventoAgregables
+                    .Where(ea => overlappingEventoIds.Contains(ea.EventoId))
+                    .GroupBy(ea => ea.AgregableId)
+                    .Select(group => new
+                    {
+                        AgregableId = group.Key,
+                        ReservedTotal = group.Sum(ea => ea.CantidadReservada)
+                    })
+                    .ToList();
+
+            foreach (EventoAgregable eventoAgregable in newEvento.EventoAgregables)
+            {
+                var matchingReserved =
+                    totalReservedAgregables.FirstOrDefault(e =>
+                        e.AgregableId == eventoAgregable.AgregableId);
+
+                if (matchingReserved != null)
+                {
+                    int reservedSum = eventoAgregable.CantidadReservada + matchingReserved.ReservedTotal;
+
+                    if (reservedSum > eventoAgregable.Agregable.Total)
+                    {
+                        // Reserved Agregables exceed the total available for at least one Agregable.
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
     }
 }
