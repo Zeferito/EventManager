38927b2997e789eba63de6aaae1279e0989f77ba
Zeferito <85717949+Zeferito@users.noreply.github.com>
2023-10-24 21:50:37 -0700

    Rename nodes for better readability

--- EventManager.Desktop/Scenes/ConfirmacionEvento/Components/Scripts/HandleRegistrationButtons.cs
+++ EventManager.Desktop/Scenes/ConfirmacionEvento/Components/Scripts/HandleRegistrationButtons.cs
@@ -3,24 +3,24 @@ using System;
 
 public partial class HandleRegistrationButtons : Node
 {
-    [Export]
-    private Button _buttonRegister;
-    [Export]
-    private Button _buttonCancel;
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        _buttonRegister.Pressed += () =>
-        {
-            GetTree().ChangeSceneToFile("res://Scenes/Inicio/scena_pantalla_inicio.tscn");
-        };
+	[Export]
+	private Button _buttonRegister;
+	[Export]
+	private Button _buttonCancel;
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		_buttonRegister.Pressed += () =>
+		{
+			GetTree().ChangeSceneToFile("res://Scenes/Inicio/scena_pantalla_inicio.tscn");
+		};
 
-        _buttonCancel.Pressed += () =>
-        {
-            GetTree().ChangeSceneToFile("res://Scenes/AddEvento/add_evento_scene.tscn");
-        };
-    }
+		_buttonCancel.Pressed += () =>
+		{
+			GetTree().ChangeSceneToFile("res://Scenes/AddEvento/add_evento_scene.tscn");
+		};
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta) { }
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta) { }
 }
