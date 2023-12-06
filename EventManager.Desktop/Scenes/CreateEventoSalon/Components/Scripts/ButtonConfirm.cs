59810ed924f5ecb5ef3af5d502ec7b6bc6ddaac3
Alec Demian Santana Celaya <73375255+SantanaAlec@users.noreply.github.com>
2023-12-05 17:50:57 -0700

    Fix dateformat

--- EventManager.Desktop/Scenes/CreateEventoSalon/Components/Scripts/ButtonConfirm.cs
+++ EventManager.Desktop/Scenes/CreateEventoSalon/Components/Scripts/ButtonConfirm.cs
@@ -1,9 +1,11 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text.Json;
 using EventManager.Desktop.Api.Dto;
+using EventManager.Desktop.Scenes.Autoload.Scripts;
 using Godot;
-using static EventManager.Desktop.Api.Entities.Event;
+using Godot.Collections;
 
 namespace EventManager.Desktop.Scenes.CreateEventoSalon.Components.Scripts;
 
@@ -37,9 +39,22 @@ public partial class ButtonConfirm : Button
     public override void _Ready()
     {
         Global global = GetNode<Global>("/root/Global");
+        ApiConnection apiConnection = GetNode<ApiConnection>("/root/ApiConnection");
 
         Pressed += () =>
         {
+            DateTime parsedStartDate;
+            if (!DateTime.TryParse(_lineEditStartDateEvent.Text, out parsedStartDate))
+            {
+                return;
+            }
+
+            DateTime parsedEndDate;
+            if (!DateTime.TryParse(_lineEditEndDateEvent.Text, out parsedEndDate))
+            {
+                return;
+            }
+
             List<int> clientIds = new();
             List<int> employeeIds = new();
             List<int> roomIds = new();
@@ -71,13 +86,13 @@ public partial class ButtonConfirm : Button
                 materialsReserved.Add(materialReserved);
             }
 
-            EventDto evento = new EventDto
+            EventDto eventDto = new EventDto
             {
                 Status = "active",
                 Name = _lineEditTitleEvent.Text,
                 Description = _textEditDescriptionEvent.Text,
-                StartDate = _lineEditStartDateEvent.Text,
-                EndDate = _lineEditEndDateEvent.Text,
+                StartDate = parsedStartDate.ToString("o", CultureInfo.InvariantCulture),
+                EndDate = parsedEndDate.ToString("o", CultureInfo.InvariantCulture),
                 UserId = 1,
                 ClientIds = clientIds,
                 EmployeeIds = employeeIds,
@@ -85,12 +100,52 @@ public partial class ButtonConfirm : Button
                 MaterialsReserved = materialsReserved
             };
 
-            global.EventToSend = evento;
+            HttpRequest httpRequest = new HttpRequest();
+            httpRequest.UseThreads = true;
+            AddChild(httpRequest);
+            httpRequest.RequestCompleted += HttpRequestCompleted;
+            httpRequest.RequestCompleted += (result, code, strings, body) =>
+            {
+                RemoveChild(httpRequest);
+                httpRequest.QueueFree();
+            };
 
-            GetTree()
-                .ChangeSceneToFile(
-                    "res://Scenes/ConfirmacionEvento/confirmacion_evento_scene.tscn"
-                );
+            string contentType = "application/json";
+            string authToken = apiConnection.AuthDetails.AuthToken;
+            string[] headers =
+            {
+                $"Content-Type: {contentType}",
+                $"Authorization: Bearer {authToken}"
+            };
+
+            string body = JsonSerializer.Serialize(eventDto);
+
+            Error error = httpRequest.Request($"{apiConnection.Url}/events", headers, HttpClient.Method.Post, body);
+
+            if (error != Error.Ok)
+            {
+                GD.PushError("An error occurred in the HTTP request.");
+            }
         };
+    }
+
+    private void HttpRequestCompleted(long result, long responseCode, string[] headers, byte[] body)
+    {
+        Json json = new Json();
+        json.Parse(body.GetStringFromUtf8());
+
+        Godot.Collections.Array responseArray = json.Data.AsGodotArray();
+        Dictionary responseDictionary = json.Data.AsGodotDictionary();
+
+        switch (responseCode)
+        {
+            case 201:
+                GD.Print(responseDictionary);
+                GetTree().ChangeSceneToFile("res://Scenes/Inicio/scena_pantalla_inicio.tscn");
+                break;
+            default:
+                GD.PrintErr(responseDictionary);
+                break;
+        }
     }
 }
